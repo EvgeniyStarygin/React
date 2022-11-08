0a43f73bf55423d1efaba454ae2711151709f872
“Evgeniy“ <zzzzz-123@inbox.ru>
2022-11-08 09:57:14 +0300

    rainbox

---
+++ rainbow/App.js
@@ -0,0 +1,13 @@
+﻿import React from "react";
+import ReactDOM from "react-dom";
+
+import Rainbow from "./components/Rainbow";
+
+let colors = ['red', 'orange', 'yellow', 'green', '#00BFFF', 'blue', 'purple'];
+
+ReactDOM.render(
+  <Rainbow colors={colors}>
+    Hello!
+  </Rainbow>,
+  document.getElementById("container")
+);
