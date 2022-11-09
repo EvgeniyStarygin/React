02fb58f20aea22774e4e5e8a1e8f2853608c6558
“Evgeniy“ <zzzzz-123@inbox.ru>
2022-11-09 15:59:27 +0300

    rainbow HOC

--- rainbowHOC/components/withRainbowFrame.js
+++ rainbowHOC/components/withRainbowFrame.js
@@ -1,16 +1,5 @@
 ﻿import React from 'react';
 
-// вариант с возвратом классового компонента
-// function withColorBackground(color) {
-//     return function(Comp) {
-//       return props => (
-//         <div style={{backgroundColor:color,border:"solid red 1px"}}>
-//           <Comp {...props} />
-//         </div>
-//       );
-//     };
-// }
-
 let withRainbowFrame = colors => Comp => props =>
   colors.reduce((acc, color) => (<div style={{ padding: "20px", border: `5px solid ${color}` }}>{acc}</div>), (<div style={{ textAlign: "center" }}> <Comp {...props} /></div>))
 
