ba7dfffa5996f7fd883d9edcbb65f62ff9da1666
EvgeniyStarygin <49728703+EvgeniyStarygin@users.noreply.github.com>
2022-11-16 10:02:36 +0300

    Delete rainbowHOC directory

--- rainbowHOC/components/withRainbowFrame.js
+++
@@ -1,7 +0,0 @@
-﻿import React from 'react';
-
-let withRainbowFrame = colors => Comp => props =>
-  colors.reduce((acc, color) => (<div style={{ padding: "20px", border: `5px solid ${color}` }}>{acc}</div>), (<div style={{ textAlign: "center" }}> <Comp {...props} /></div>))
-
-
-export { withRainbowFrame };
